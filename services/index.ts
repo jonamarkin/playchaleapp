a131a5e04b9fc7c246e4c8933e37153b8cb1784c
Jonathan Ato Markin <jonamarkin@gmail.com>
2026-05-29 16:20:45 +0200

    Build fixes

--- services/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export all services
-export { api, ApiError, isMockBackend, setAuthToken, type ApiResponse } from './api';
-export { backend, backendMode } from './backend';
-export { httpBackend } from './httpBackend';
-export type { BackendClient } from './backendTypes';
-export * from './games';
-export * from './players';
